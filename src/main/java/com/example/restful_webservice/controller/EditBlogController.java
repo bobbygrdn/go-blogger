e20015406abf1142291af858fce824f200ff51a9
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-06-09 08:31:57 -0700

    adds method to edit and save changes to blog

--- src/main/java/com/example/restful_webservice/controller/EditBlogController.java
+++ src/main/java/com/example/restful_webservice/controller/EditBlogController.java
@@ -6,22 +6,38 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.restful_webservice.model.BlogPost;
-import com.example.restful_webservice.repository.BlogPostRepository;
+import com.example.restful_webservice.service.BlogPostService;
 
 @Controller
 public class EditBlogController {
 
     @Autowired
-    private BlogPostRepository blogPostRepository;
+    private BlogPostService blogPostService;
 
     @GetMapping("/editBlog/{id}")
     public String editBlog(@PathVariable("id") long id, Model model) {
 
-        BlogPost blogPost = blogPostRepository.findById(id).get();
+        BlogPost blogPost = blogPostService.getBlogPost(id);
 
         model.addAttribute("blogPost", blogPost);
 
         return "editBlog";
+    }
+
+    @PutMapping("/editBlog/{id}")
+    public String updateBlog(@PathVariable("id") long id, @RequestParam("title") String title,
+            @RequestParam("content") String content) {
+
+        BlogPost existingBlogPost = blogPostService.getBlogPost(id);
+
+        if (existingBlogPost == null) {
+            return "redirect:/myBlogs";
+        }
+
+        existingBlogPost.setTitle(title);
+        existingBlogPost.setContent(content);
+
+        return "redirect:/myBlogs";
     }
 
 }
