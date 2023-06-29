8aeeb0048826ca6ae1db6192b84eb20fb68d41a5
Robert <96712943+bobbygrdn@users.noreply.github.com>
2023-06-29 08:24:31 -0700

    Removes custom post mapped login method

--- src/main/java/com/example/restful_webservice/pages/LoginController.java
+++ src/main/java/com/example/restful_webservice/pages/LoginController.java
@@ -1,44 +1,15 @@
 package com.example.restful_webservice.pages;
 
-import javax.servlet.http.HttpSession;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
-
     @GetMapping("/login")
     public String ShowLogin(Model model) {
         return "login";
-    }
-
-    @PostMapping("/login")
-    public String login(HttpSession session, @RequestParam("username") String username,
-            @RequestParam("password") String password,
-            Model model) {
-        try {
-            Authentication authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(username, password));
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-            session.setAttribute("username", username);
-            return "redirect:/home";
-        } catch (AuthenticationException e) {
-            model.addAttribute("error", "Invalid username or password");
-            return "login";
-        }
     }
 
     @GetMapping("/logout")
